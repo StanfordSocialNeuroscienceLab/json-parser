5c2b26d2f1074486588ffce928fdc9e0e36e5df9
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-07-11 16:39:42 -0700

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -25,10 +25,12 @@ def checkCommandLine():
 
     ux = sys.argv
 
+    # User didn't include key and JSON
     if len(ux) < 2:
         print("\nHeck! Invalid user input\n\nYour input:\t{}\n".format(ux))
         sys.exit(1)
 
+    # Incorrect order of arguments (JSON files come last)
     for userEntry in ux[2:]:
         if ".json" not in userEntry:
             print("\nInvalid user entry - run python3 main.py [ KEY VALUE ] [ JSON FILE ]\n")
@@ -42,9 +44,9 @@ def parseUserInput():
     Returns string object (desired key) and list object (list of JSONs, may only be one)
     """
 
-    ux = sys.argv
-    keyName = ux[1]
-    JSON_files = ux[2:]
+    ux = sys.argv                                                       # List of command line args
+    keyName = ux[1]                                                     # Key val must come before JSON files
+    JSON_files = ux[2:]                                                 # May be 1 or more JSON files
 
     return keyName, JSON_files
 
@@ -55,26 +57,17 @@ def directoryTree(KEY):
     Makes output directory AND returns string value of relative path
     """
 
-    path = ("./OUTPUT/{}/".format(KEY))
+    path = ("./OUTPUT/{}/".format(KEY))                                 # Relative output path
 
+    # If Output parent dir doesn't exist at all..
     if not os.path.isdir("./OUTPUT/"):
         os.mkdir("./OUTPUT/")
 
+    # Make subdir within Output dir
     if not os.path.isdir(path):
         os.mkdir(path)
 
     return path
-
-
-def checkForCopies(FULLPATH):
-    """
-    Prevents file overwriting over multiple runs
-    """
-
-    if os.path.isfile(FULLPATH):
-        return True
-    else:
-        return False
 
 
 def outputName(KEYVALUE, JSON):
@@ -82,9 +75,10 @@ def outputName(KEYVALUE, JSON):
     Returns name of output JSON file
     """
 
-    temp = str(JSON)[:-5]
-    temp = temp.split('/')[-1]
-    temp += "_{}-only.json".format(KEYVALUE)
+    temp = str(JSON)[:-5]                                               # Isolate filename without extension
+    temp = temp.split('/')[-1]                                          # Strip leading directory path
+    temp += "_{}-only.json".format(KEYVALUE)                            # Append to filename
+
     return temp
 
 
@@ -93,14 +87,14 @@ def isoUSER(KEYVALUE, JSON, PATH):
     Reduces JSON file down to desired key tree
     """
 
-    newName = outputName(KEYVALUE, JSON)
+    newName = outputName(KEYVALUE, JSON)                                # Generate output filename
 
     with open(JSON) as incoming:
-        temp = json.load(incoming)
-        temp = temp[KEYVALUE]
+        temp = json.load(incoming)                                      # Read JSON as dictionary
+        temp = temp[KEYVALUE]                                           # Reduce JSON to desired key
 
     with open(os.path.join(PATH, newName), "w") as outgoing:
-        json.dump(temp, outgoing, indent=4)
+        json.dump(temp, outgoing, indent=4)                             # Write JSON to Output dir
 
 
 def main():
@@ -108,10 +102,11 @@ def main():
     Let's get it
     """
 
-    checkCommandLine()
-    user_value, json_list = parseUserInput()
-    path = directoryTree(user_value)
+    checkCommandLine()                                                  # Confirm arguments supplied
+    user_value, json_list = parseUserInput()                            # Isolate arguments
+    path = directoryTree(user_value)                                    # Save output path to memory
     
+    # In case multiple JSONs are provided...
     for k in json_list:
         print("Subsetting {}....".format(k))
         isoUSER(user_value, k, path)
