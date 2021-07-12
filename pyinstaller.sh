0c25aac21afc5852acdb5929c55188b9b4023466
Ian <47256454+IanRFerguson@users.noreply.github.com>
2021-07-12 10:53:34 -0700

    Update pyinstaller.sh

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -5,17 +5,25 @@
 #
 # Ian Richard Ferguson | Stanford University
 
-# Install Homebrew
-curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+LINK="https://raw.githubusercontent.com/Homebrew/install/master/install.sh"     # Install Homebrew
+curl -fsSL -o install.sh $LINK
 /bin/bash install.sh
 
-# Update shell profile
-export PATH=/usr/local/bin:$PATH >> ~/.bash_profile
+export PATH=/usr/local/bin:$PATH >> ~/.bash_profile                             # Update shell profile (Bash & ZSH)
 export PATH=/usr/local/bin:$PATH >> ~/.zshrc
 
 source ~/.bash_profile
 source ~/.zshrc
 
-# Install Python3 and JupyterLab
-brew install python
+brew install python                                                             # Install Python3 and JupyterLab
 brew install jupyterlab
+
+brew unlink python && brew link python                                          # Re-link Python in case of any systematic issues with your local env
+
+packages=("pandas" "numpy" "scikit-learn" "matplotlib" "seaborn")               # Installs several useful Python packages
+
+for k in ${packages[@]}; do
+  pip install $k
+done
+
+rm install.sh                                                                   # I was never here ...
